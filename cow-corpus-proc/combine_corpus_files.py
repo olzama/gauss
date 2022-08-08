ac665bd93f24a5f1b81d2fa43f0cdd0add19e874
Olga Zamaraeva <olzama@uw.edu>
2022-08-08 12:58:16 +0200

    Added corpus processing folder

---
+++ cow-corpus-proc/combine_corpus_files.py
@@ -0,0 +1,14 @@
+import sys
+import glob
+
+path_to_corpus = sys.argv[1]
+
+def collect_all_raw(d):
+    pass
+
+def collect_all_annotated(d):
+    pass
+
+def collect_per_course_level(d):
+    pass
+
