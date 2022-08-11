c2ac53d477784536409f2fa831d10d232941e1c3
Olga Zamaraeva <olzama@uw.edu>
2022-08-11 16:24:14 +0200

    started corpus traversing script

--- cow-corpus-proc/combine_corpus_files.py
+++ cow-corpus-proc/combine_corpus_files.py
@@ -1,7 +1,15 @@
-import sys
-import glob
+'''
+This script assumes the directory structure and file format and conventions
+from the COW-SL second language learner corpus of Spanish
+developed at UC Davis: https://github.com/ucdaviscl/cowsl2h
 
-path_to_corpus = sys.argv[1]
+The purpose of the script:
+Combine COW-SL corpus files in to a single file, one sentence per line.
+'''
+
+import sys
+from pathlib import Path
+
 
 def collect_all_raw(d):
     pass
@@ -12,3 +20,14 @@ def collect_all_annotated(d):
 def collect_per_course_level(d):
     pass
 
+'''
+Traverse d recursively, looking for folders named k.
+Return a single string containing all texts from all k-named folders.   
+'''
+def find_relevant_folder(d, k):
+    pass
+
+if __name__ == "__main__":
+    path_to_corpus = Path(sys.argv[1])
+    for p in path_to_corpus.rglob("*"):
+        print(p)
