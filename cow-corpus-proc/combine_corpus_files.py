81bff9e8bddd0ee03e676174518e6d71679db60e
suarezallegue <lorenasasd@gmail.com>
2023-06-13 11:17:07 +0200

    collect_all_sentences returns a dictionary

--- cow-corpus-proc/combine_corpus_files.py
+++ cow-corpus-proc/combine_corpus_files.py
@@ -26,39 +26,29 @@ def find_relevant_folders(d, k):
     folders = glob.glob(d + "**/**/" + k, recursive=True)
     return folders
 
-'''
-Save all texts from a list of folders in a list.
-'''
-def collect_all_sentences (relevant_folders):
+def collect_all_sentences(relevant_folders):
     sentences = []
+    dictionary = {}
 
     for fol in relevant_folders:
-        if fol.endswith('annotated'):
+        if not fol.endswith("annotated"):
+            textfiles = glob.glob(fol + '/*.txt')
+        else:
             issues = glob.glob(fol + '/**')
             for issue in issues:
-                if issue.endswith('gender_number'):
-                    for textfile in glob.glob(issue + '/**/*.txt'):# Iterate over all files with extension .txt in the directory named fol
-                        file_name = os.path.basename(textfile)
-                        with open(textfile, 'r') as f:
-                            text = f.read()
-                            sent_tokenized_text = nltk.sent_tokenize(text, language='spanish')  # Split text into sentences using the NLTK tokenizer
-                        sentences.extend(sent_tokenized_text)
-        else:
-            files = glob.glob(fol + '/*.txt')
-            for textfile in files: # Iterate over all files with extension .txt in the directory named fol
-                with open(textfile, 'r') as f:
-                    file_name = os.path.basename(textfile)
-                    text = f.read()
-                sent_tokenized_text = nltk.sent_tokenize(text, language='spanish') # Split text into sentences using the NLTK tokenizer
-                sentences.extend(sent_tokenized_text) # Add the sentences to the list such that it is a flat list (not a list of list; compare to using "append" instead of "extend")
+                if issue.endswith("gender_number"):
+                    textfiles = glob.glob(issue + '/**/*.txt')
+        for textfile in textfiles:
+            file_name = os.path.basename(textfile)
+            with open(textfile, 'r') as f:
+                text = f.read()
+            sent_tokenized_text = nltk.sent_tokenize(text, language='spanish')
+            sentences.extend(sent_tokenized_text)
+            dictionary[file_name] = {}
+            for i, sentence in enumerate(sent_tokenized_text):
+                dictionary[file_name][i] = sentence
 
-    return sentences
-
-
-def output_string(id, sentence):
-    output = str(id) + '@' + str(sentence['number']) + '@' + str(sentence['input']) + '\n'
-    return output
-
+    return dictionary
 
 if __name__ == "__main__":
     path_to_corpus = sys.argv[1]  # sys.argv[1] is the first argument passed to the program through e.g. pycharm (or command line). In Pycharm, look at Running Configuration
@@ -69,7 +59,6 @@ if __name__ == "__main__":
     print('Working with corpus {}'.format(path_to_corpus))
     essays = find_relevant_folders(path_to_corpus, "essays")
     print('Found {} folders named essays in the corpus.'.format(len(essays)))
-
     '''
     Save all sentences from all selected files in a single file.
     Sanity check report.
@@ -77,9 +66,11 @@ if __name__ == "__main__":
     print('Looking for sentences in essays.')
     raw_sentences = collect_all_sentences(essays)
     print('Total {} raw sentences.'.format(len(raw_sentences)))
+
     with open('output/COWSL2H_essays.txt', 'w') as f:
-        for sentence in raw_sentences:
-            f.write(sentence + '\n')
+        for file, sentences in raw_sentences.items():
+            for num, sen in sentences.items():
+                f.write(str(file) + '@' + str(num) + '@' + str(sen) + '\n')
 
     """
     Find folders containing annotated essays in the corpus.
@@ -94,10 +85,11 @@ if __name__ == "__main__":
     '''
     print('Compiling sentences from {} annotated folders.'.format(len(annotated)))
     annotated_sentences = collect_all_sentences(annotated)
-    print('Total {} annotated sentences in the corpus.'.format(len(annotated_sentences)))
+    # # print('Total {} annotated sentences in the corpus.'.format(len(annotated_sentences)))
     with open('output/COWSL2H_annotated.txt', 'w') as f:
-        for sentence in annotated_sentences:
-            f.write(sentence + '\n')
+        for file, sentences in annotated_sentences.items():
+            for num, sen in sentences.items():
+                f.write(str(file) + '@' + str(num) + '@' + str(sen) + '\n')
 
     '''
     Find folders named "corrected" in the corpus.
@@ -114,5 +106,6 @@ if __name__ == "__main__":
     corrected_sentences = collect_all_sentences(corrected)
     print('Total {} corrected sentences.'.format(len(corrected_sentences)))
     with open("output/COWSL2H_corrected.txt", 'w') as f:
-        for sentence in corrected_sentences:
-            f.write(sentence + '\n')
+        for file, sentences in corrected_sentences.items():
+            for num, sen in sentences.items():
+                f.write(str(file) + '@' + str(num) + '@' + str(sen) + '\n')
