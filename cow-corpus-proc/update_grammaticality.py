9360ef5082605bcb5d9b4990df053c22ef9c09f2
olzama <olga.zamaraeva@gmail.com>
2023-11-09 13:05:09 +0100

    Take only the first two columns in the tab-separated file: the sentence and the grammaticality judgment. There is also an optional comment in the third column that can be ignored.

--- cow-corpus-proc/update_grammaticality.py
+++ cow-corpus-proc/update_grammaticality.py
@@ -21,7 +21,7 @@ A sentence of length 4, counting punctuation, will be inder the directory 4/.
 def update_testsuite(ts, grammaticality_judgments):
     for i, row in enumerate(ts['item']):
         sentence_text = row['i-input']
-        s, grammaticaly_judgment = grammaticality_judgments[i].split('\t')
+        s, grammaticaly_judgment = grammaticality_judgments[i].split('\t')[:2] #May also contain a comment in the last col
         assert sentence_text == s
         ts['item'].update(i, {'i-wf': grammaticaly_judgment})
     ts.commit()
