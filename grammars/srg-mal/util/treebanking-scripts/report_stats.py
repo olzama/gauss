eda185d0eb284a979f73d8c001dae4d33c3b812d
suarezallegue <lorenasasd@gmail.com>
2024-01-23 11:43:31 +0100

    reduced code overlap. added a general rule count

--- grammars/srg-mal/util/treebanking-scripts/report_stats.py
+++ grammars/srg-mal/util/treebanking-scripts/report_stats.py
@@ -19,10 +19,6 @@ def report_stats(treebanks_path):
             sentences.append(response['i-input'])
             # In a thinned parsed forest, results will be empty if the item was not accepted as correct in treebanking.
             if len(response['results']) > 0:
-                # try looking at response['results'][0] to see which phrase structure rules were used
-                r0 = response['results'][0]
-                # A derivation is a tree that consists of nodes. Nodes can be phrase structure rules or lexical rules or "terminals" (words).
-                deriv = response.result(0).derivation() # Exercise: Try to find in the pydelphin docs how to get a flat list of the derivation nodes.
                 accepted.append(response['i-input'])
                 all_accepted.append(response['i-input'])
             else:
@@ -34,37 +30,24 @@ def report_stats(treebanks_path):
     acc = len(all_accepted) / len(all_sentences)
     print('Total accuracy: {} out of {} ({:.2f})'.format(len(all_accepted), len(all_sentences), acc))
 
-def report_stats2(treebanks_path):
-    all_sentences = []
-    all_accepted = []
-    all_rejected = []
+def report_rule_counts(treebanks_path):
+    all_rules_count = {}
     for i, tsuite in enumerate(sorted(glob.iglob(treebanks_path + '/**'))):
-        sentences = []
-        accepted = []
-        rejected = []
         ts = itsdb.TestSuite(tsuite)
         items = list(ts.processed_items())
-        #print("{} sentences in corpus {} including possible sentences with no parse.".format(len(items), ts.path.stem))
         for response in items:
-            all_sentences.append(response['i-input'])
-            sentences.append(response['i-input'])
-            # In a thinned parsed forest, results will be empty if the item was not accepted as correct in treebanking.
             if len(response['results']) > 0:
                 # try looking at response['results'][0] to see which phrase structure rules were used
                 r0 = response['results'][0]
                 # A derivation is a tree that consists of nodes. Nodes can be phrase structure rules or lexical rules or "terminals" (words).
-                deriv = response.result(0).derivation() # Exercise: Try to find in the pydelphin docs how to get a flat list of the derivation nodes.
-                phsr_counts = count_phsr(deriv)
-                accepted.append(response['i-input'])
-                all_accepted.append(response['i-input'])
-            else:
-                #print('Rejected: {}'.format(response['i-input']))
-                rejected.append(response['i-input'])
-                all_rejected.append(response['i-input'])
-        acc = len(accepted)/len(sentences)
-        print('Corpus {} accuracy {} out of {} ({:.2f})'.format(ts.path.stem, len(accepted), len(sentences), acc))
-    acc = len(all_accepted) / len(all_sentences)
-    print('Total accuracy: {} out of {} ({:.2f})'.format(len(all_accepted), len(all_sentences), acc))
+                deriv = derivation.from_string(r0['derivation']) # Exercise: Try to find in the pydelphin docs how to get a flat list of the derivation nodes.
+                rule_count = count_phsr(deriv)
+                # Collect rule counts from each test suite and store them in one dictionary.
+                for phsr, count in rule_count.items():
+                    all_rules_count[phsr] = all_rules_count.get(phsr, 0) + count
+                    #print('{} constraints used.'.format(len(rule_count)))
+                    #print('{} rule used {} times.'.format(phsr, count))
+    print('Total {} constraints used.'.format(len(all_rules_count)))
 
 def count_phsr(d):
     counts = {}
@@ -80,5 +63,5 @@ def count_phsr(d):
     return counts
 
 if __name__ == '__main__':
-    #report_stats(sys.argv[1])
-    report_stats2(sys.argv[1])
+    report_stats(sys.argv[1])
+    report_rule_counts(sys.argv[1])
