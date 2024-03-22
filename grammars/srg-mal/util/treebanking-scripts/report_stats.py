d9a5cdedb65e4c30cd0a6868a539a11f0f7768c1
suarezallegue <lorenasasd@gmail.com>
2024-03-22 12:39:09 +0100

    added a function to sort rules in academic courses

--- grammars/srg-mal/util/treebanking-scripts/report_stats.py
+++ grammars/srg-mal/util/treebanking-scripts/report_stats.py
@@ -1,3 +1,4 @@
+import re
 import sys
 from delphin import itsdb, derivation
 import glob
@@ -31,27 +32,67 @@ def report_stats(treebanks_path):
     print('Total accuracy: {} out of {} ({:.2f})'.format(len(all_accepted), len(all_sentences), acc))
 
 def report_rule_counts(treebanks_path):
-    all_rules_count = {}
-    rule_count_list = []
+    rules_count = {}
+    rules = []
     for i, tsuite in enumerate(sorted(glob.iglob(treebanks_path + '/**'))):
         ts = itsdb.TestSuite(tsuite)
-        items = list(ts.processed_items())
-        for response in items:
+        responses = list(ts.processed_items())
+        for response in responses:
             if len(response['results']) > 0:
                 # try looking at response['results'][0] to see which phrase structure rules were used
                 r0 = response['results'][0]
                 # A derivation is a tree that consists of nodes. Nodes can be phrase structure rules or lexical rules or "terminals" (words).
                 deriv = derivation.from_string(r0['derivation']) # Exercise: Try to find in the pydelphin docs how to get a flat list of the derivation nodes.
                 rule_count = count_rules(deriv)
-                rule_count_list.append(rule_count)
+                if rule_count not in rules:
+                    rules.append(rule_count)
         # Collect rule counts from each test suite and store them in one dictionary.
         for phsr, info in rule_count.items():
-            if info['is_root'] is False:
-                all_rules_count[phsr] = sum(info['counts'] for rule_count in rule_count_list if phsr in rule_count)
-    for k, v in sorted(all_rules_count.items(), key=lambda item:item[1], reverse=True):
-        print('Rule {}: {} uses.'.format(k, v))
-    print('Total number of constraints used: {}'.format(sum(all_rules_count.values())))
-    print('{} different constraints used.'.format(len(all_rules_count)))
+            if phsr not in rules_count and info['is_root'] is False:
+                    rules_count[phsr] = sum(info['counts'] for rule_count in rules if phsr in rule_count)
+    for k, v in sorted(rules_count.items()):
+        print('{} used {} times'.format(k, v))
+    print('Total number of constraints used: {}'.format(len(rules_count)))
+
+def report_sorted(treebanks_path):
+    sorted_rules = {}
+    for i, tsuite in enumerate(sorted(glob.iglob(treebanks_path + '/**'))):
+        ts = itsdb.TestSuite(tsuite)
+        items = list(ts.processed_items())
+        sorted_items = sort_by_course(items)
+        for course, responses in sorted_items.items():
+            rules = []
+            if course not in sorted_rules:
+                sorted_rules[course] = {}
+            for response in sorted_items[course]:
+                if len(response['results']) > 0:
+                    r0 = response['results'][0]
+                    deriv = derivation.from_string(r0['derivation']) 
+                    rule_count = count_rules(deriv)
+                    rules.append(rule_count)
+            for phsr, info in rule_count.items():
+                if phsr not in sorted_rules[course] and info['is_root'] is False:
+                    sorted_rules[course][phsr] = sum(info['counts'] for rule_count in rules if phsr in rule_count)
+    for course, data in sorted(sorted_rules.items()):
+        print('\n {} rules in {}'.format(len(data), course))
+        for rule, num in sorted(data.items()):
+            print('{} used {} times'.format(rule, num))
+
+
+def sort_by_course(list):
+    #Look at response['i-commment'] to extract the course in which the author was enrolled. Sort the treebanks in a dictionary
+    sorted_items = {}
+    pattern = re.compile("course: (\w+ \d)")
+    for response in list:
+        comment = response['i-comment']
+        matches = re.search(pattern, comment)
+        assert matches
+        k = matches.group(1)
+        if k not in sorted_items:
+            sorted_items[k] = []
+        sorted_items[k].append(response)
+    return sorted_items
+
 
 
 def count_rules(d):
@@ -69,3 +110,4 @@ def count_rules(d):
 if __name__ == '__main__':
     report_stats(sys.argv[1])
     report_rule_counts(sys.argv[1])
+    report_sorted(sys.argv[1])
