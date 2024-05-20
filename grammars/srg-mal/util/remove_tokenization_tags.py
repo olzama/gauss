352014ad5f20ba6007a959d1a3d48b790d85bcf9
suarezallegue <lorenasasd@gmail.com>
2024-05-20 13:26:52 +0200

    added a function to report number of items left

--- grammars/srg-mal/util/remove_tokenization_tags.py
+++ grammars/srg-mal/util/remove_tokenization_tags.py
@@ -14,6 +14,20 @@ def process_directory(directory_path):
                     new_line = re.sub(r'((?:[^@]*@){7})[^@]*(@)', r'\1\2', line)
                     f.write(new_line)
             print(f"Processed file: {filepath}")
+
+def count_items(folder):
+    filename = "item"
+    filepaths = glob.iglob(folder + '/**/**/' + filename)
+    items = []
+    for path in filepaths:
+        if os.path.isfile(path):
+            with open(path, 'r') as f:
+                sentences = f.readlines()
+                for sentence in sentences:
+                    items.append(sentence)
+    print('{} items left'.format(len(items)))
+
 if __name__ == '__main__':
     directory_path = sys.argv[1] # Expected input: /path-to-folder/test/
-    process_directory(directory_path)
+    #process_directory(directory_path)
+    count_items(directory_path)
